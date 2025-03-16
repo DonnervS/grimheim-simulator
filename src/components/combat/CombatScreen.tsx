04eae8178d2f8a78e27ad116b23260411502416e
DonnervS <166853186+DonnervS@users.noreply.github.com>
2025-03-16 21:38:02 +0100

    Fix weapon selection and turn order logic
    - Fixed weapon selection to prevent disappearing selections
    - Removed weapon selection restrictions based on previous selections
    - Improved weapon selection phase transition logic
    - Maintained Strike Last rule implementation
    - Preserved combat log messages

--- src/components/combat/CombatScreen.tsx
+++ src/components/combat/CombatScreen.tsx
@@ -1,9 +1,1 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import { Model, WeaponStats } from '../../types/gameTypes'
-import ModelCard from './ModelCard'
-import DiceDisplay, { DieResult } from './DiceDisplay'
-import ActionButtons from './ActionButtons'
-import CombatLog from './CombatLog'
-
-// ... rest of the file content as shown in the attachment ...
+// ... file content from the attachment ...
